80a31a0cc1874ba584530b19f6ec2e67f304c64e
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-06-08 12:05:08 +0200

    Update GetMovies.js

--- src/api/endpoints/GetMovies.js
+++ src/api/endpoints/GetMovies.js
@@ -1,7 +1,7 @@
 import apiRequest from "../ApiRequest.js";
 
 async function getMovies() {
-    const response = await apiRequest("http://localhost:5000/movie/readall", "GET");
+    const response = await apiRequest("http://localhost:5000/api/movie", "GET");
     if(response.ok) {
         return response.json();
     }
